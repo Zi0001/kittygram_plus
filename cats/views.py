3c8bc8d86ba7bd93e184e3c1eb0bf222e229263e
Zi0001 <WorckAcTop.218@gmail.com>
2024-10-26 15:33:20 +0300

    kitty

--- cats/views.py
+++ cats/views.py
@@ -1,9 +1,54 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.decorators  import action
+from rest_framework.response  import Response
+from rest_framework import mixins
 
-from .models import Cat
-from .serializers import CatSerializer
+from .models import Cat, Owner
+from .serializers import CatSerializer, CatListSerializer, OwnerSerializer
 
 
-class CatViewSet(viewsets.ModelViewSet):
+class CatViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Cat.objects.all()
+        serializer = CatSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Cat.objects.all()
+        cat = get_object_or_404(queryset, pk=pk)
+        serializer = CatSerializer(cat)
+        return Response(serializer.data)
+
+    # Пишем метод, а в декораторе разрешим работу со списком объектов
+    # и переопределим URL на более презентабельный
+    @action(detail=False, url_path='recent-white-cats')
+    def recent_white_cats(self, request):
+        # Нужно получить записи о пяти котиках белого цвета
+        cats = Cat.objects.filter(color='White')[:5]
+        # Передадим queryset cats сериализатору 
+        # и разрешим работу со списком объектов
+        serializer = self.get_serializer(cats, many=True)
+        return Response(serializer.data)
+
+    def get_serializer_class(self):
+        # Если запрошенное действие (action) — получение списка объектов ('list')
+        if self.action == 'list':
+            # ...то применяем CatListSerializer
+            return CatListSerializer
+        # А если запрошенное действие — не 'list', применяем CatSerializer
+        return CatSerializer
+
+
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+
+
+class CreateRetrieveViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
+    # В теле класса никакой код не нужен! Пустячок, а приятно.
+    pass
+
+class LightCatViewSet(CreateRetrieveViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
